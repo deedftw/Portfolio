e237804916a091b32279a0216dbac585d5f532e2
Georgi Stoyanov Ovalov <georgiovalov97@gmail.com>
2020-06-10 16:05:11 +0200

    Adding a bit of functionality and working on layout

---
+++ index.js
@@ -0,0 +1,32 @@
+// expand and collapse feature
+
+const fnmap = {
+    'toggle': 'toggle',
+      'show': 'add',
+      'hide': 'remove'
+  };
+  const collapse = (selector, cmd) => {
+    const targets = Array.from(document.querySelectorAll(selector));
+    targets.forEach(target => {
+      target.classList[fnmap[cmd]]('show');
+    });
+  }
+    
+   
+  const triggers = Array.from(document.querySelectorAll('[data-toggle="collapse"]'));
+  
+  window.addEventListener('click', (ev) => {
+    const elm = ev.target;
+    if (triggers.includes(elm)) {
+      const selector = elm.getAttribute('data-target');
+      collapse(selector, 'toggle');
+    }
+  }, false);
+
+  //change the button text after collapse
+  
+  function changeTextButton()
+  {
+      this.value = (this.value == 'See more' ? 'See less' : 'See more');
+  }
+  document.getElementById('expanding-projects').onclick=changeTextButton;
